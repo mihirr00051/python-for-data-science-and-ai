990661026440937539da8ce523609984f16c046a
MIHIRR DOBARIYA <184418338+mihirr00051@users.noreply.github.com>
2026-01-10 18:20:26 +0530

    Create string_formatting.py

---
+++ CHAPTER-05: STRINGS/string_formatting.py
@@ -0,0 +1,13 @@
+# Example 1: f-string formatting
+model = "ViT"
+accuracy = 0.85
+print(f"Model {model} achieved accuracy {accuracy}")
+
+# Example 2: format() method
+name = "shree"
+field = "AI"
+print("My name is {} and I study {}".format(name, field))
+
+# Example 3: Formatting numbers
+loss = 0.123456
+print(f"Training loss: {loss:.2f}")
