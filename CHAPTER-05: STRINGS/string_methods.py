099d33fd785573c56ebdbd7360ac3a3091175479
MIHIRR DOBARIYA <184418338+mihirr00051@users.noreply.github.com>
2026-01-10 18:09:04 +0530

    Add string methods examples in string_methods.py

---
+++ CHAPTER-05: STRINGS/string_methods.py
@@ -0,0 +1,11 @@
+# Example 1: Uppercase
+language = "python"
+print(language.upper())
+
+# Example 2: Replace text
+sentence = "I love Python"
+print(sentence.replace("love", "learn"))
+
+# Example 3: Split string
+skills = "Python,Data Science,AI"
+print(skills.split(","))
