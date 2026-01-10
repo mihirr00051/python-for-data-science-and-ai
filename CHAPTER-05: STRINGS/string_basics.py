2119cd51ea3ebbded8f753274c25ce2c1040cc24
MIHIRR DOBARIYA <184418338+mihirr00051@users.noreply.github.com>
2026-01-10 18:05:32 +0530

    Add string examples in string_basics.py

---
+++ CHAPTER-05: STRINGS/string_basics.py
@@ -0,0 +1,13 @@
+# Example 1: Simple string
+name = "Mihir"
+print(name)
+
+# Example 2: String with spaces
+domain = "Data Science & AI"
+print(domain)
+
+# Example 3: Multiline string
+intro = """Python is used in
+Data Science
+and Artificial Intelligence."""
+print(intro)
